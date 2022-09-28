29794fc76f46bd8febc04bca44ce19c7519e5840
Braden Wong <bmw02002turbo@gmail.com>
2022-09-28 15:33:55 -0400

    Feat: Comments along the way

--- streamlit.py
+++ streamlit.py
@@ -12,11 +12,14 @@ While I was at London, I made an app that lets you see how much I'm thinking abo
 
 
 
+"""I exported the database and then converted it into a GeoJSON file (a file which can be visualized on a map) to double check that everything was working."""
+
 # Set map_data to the geojson.json data
 with open("geojson.json") as response:
     map_data = json.load(response)
-
 map_data
+
+"""Then I converted it into a pandas dataframe so that I could do some data manipulation."""
 
 # Turn the geojson into a pandas dataframe with a lat and lon column
 df = pd.DataFrame(map_data['features'])
@@ -24,4 +27,8 @@ df['lat'] = df['geometry'].apply(lambda x: x['coordinates'][1])
 df['lon'] = df['geometry'].apply(lambda x: x['coordinates'][0])
 df
 
+"""Finally, I used the streamlit library to create a map and a slider to visualize the data."""
+
+# Create a map and a slider to visualize the data
 st.map(df)
+st.slider("How much do you love me?", 0, 100)
