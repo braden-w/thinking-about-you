e57e30a31b2adc4f10e57ec7cb7e87e3352aeed1
Braden Wong <bmw02002turbo@gmail.com>
2022-09-28 15:54:27 -0400

    Comments

--- streamlit.py
+++ streamlit.py
@@ -8,7 +8,7 @@ Hi Carrie! This is Braden :)
 
 It's you're birthday, and I wanted to say hi and let you know that I'm thinking about you.  This is a repository of every thought I had of you during this summer :)
 
-While I was at London, I made an app which recorded the time and location whenever I thought of you and pressed a button. The goal was to let you see how much I'm thinking about you. It's a little silly, but I hope you like it (because you're architecture friends were roasting me LOL).
+While I was at London, I made an app which recorded the time and location whenever I thought of you and pressed a button. The goal was to let you see how much I'm thinking about you. It's a little silly, but I hope you like it (because your architecture friends were roasting me LOL).
 """
 
 st.image("thinking about you.jpg")
